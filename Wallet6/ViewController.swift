684d2e71e945be1191f90f261f8961f6f9233e5c
Danny Peng <Dannypengxd@gmail.com>
2018-01-20 20:55:39 -0800

    Initial Commit

---
+++ Wallet6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wallet6
+//
+//  Created by Danny Peng on 1/20/18.
+//  Copyright © 2018 Danny Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
